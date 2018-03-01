c36a620490ac21005c77f563fc629840b655fc82
kol4k <rafinyadi@gmail.com>
2018-03-01 23:45:24 +0700

    add saved

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -2,17 +2,11 @@
 
 namespace App\Http\Services;
 
-<<<<<<< HEAD
 use Illuminate\Http\Request;
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use App\Http\Controllers\Library\SusiController;
 use App\Http\Controllers\Library\YandexController;
-=======
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use App\Http\Controllers\Library\SusiController;
->>>>>>> 7276e2111611f21514468ba3396a514f29aff65f
 
 class GetMessageService
 {
@@ -28,7 +22,6 @@ class GetMessageService
      * @var susi
      */
     private $susi;
-<<<<<<< HEAD
     /**
      * @var yandex
      */
@@ -41,15 +34,6 @@ class GetMessageService
     }
     
     public function replySend(Request $r, $formData)
-=======
-
-    public function __construct(SusiController $susi)
-    {
-        $this->susi = $susi;
-    }
-    
-    public function replySend($formData)
->>>>>>> 7276e2111611f21514468ba3396a514f29aff65f
     {
         $replyToken = $formData['events']['0']['replyToken'];
         $chatText = $formData['events']['0']['message']['text'];        
@@ -57,7 +41,6 @@ class GetMessageService
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
         
-<<<<<<< HEAD
         if($chatText == '/terjemah') {
             $response = $this->bot->replyText($replyToken, 'Masukan kata kata untuk diterjemahkan, dan apabila selesai ketik: /selesai');
             $r->session()->put('query', 'terjemah');
@@ -70,9 +53,6 @@ class GetMessageService
         } else {
             $response = $this->bot->replyText($replyToken, $this->susi->getFunction($chatText));
         }
-=======
-        $response = $this->bot->replyText($replyToken, $this->susi->getFunction($chatText));
->>>>>>> 7276e2111611f21514468ba3396a514f29aff65f
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
