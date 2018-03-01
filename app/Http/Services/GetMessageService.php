1b2b5019b6d4e0bc6a12320720dffac1d576e1ad
kol4k <rafinyadi@gmail.com>
2018-03-01 22:15:23 +0700

    test 7

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -29,12 +29,11 @@ class GetMessageService
     public function replySend($formData)
     {
         $replyToken = $formData['events']['0']['replyToken'];
-        $message = $formData['events']['0']['message']['text'];
         
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
         
-        $response = $this->bot->replyText($replyToken, 'xx'.$susi->getFunction());
+        $response = $this->bot->replyText($replyToken, $this->susi->getFunction());
         
         if ($response->isSucceeded()) {
             logger("reply success!!");
--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('susi/{query}', 'Library\SusiController@getFunction');
+// Route::get('susi', 'Library\SusiController@getFunction');
 Route::get('/get-message', function (Request $request) { 
     logger("message request : ", $request->all());
 });
 Route::post('/get-message', ['as' => 'line.bot.message', 'uses' => 'GetMessageController@getMessage']);
-Route::get('x/{query}', 'GetMessageController@test');
