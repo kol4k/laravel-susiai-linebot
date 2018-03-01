ee2390382ec240324d1f9d18f119b2b78928b67a
kol4k <rafinyadi@gmail.com>
2018-03-01 21:12:53 +0700

    hello world reply

---
+++ app/Http/Controllers/Library/SusiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Library;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class SusiController extends Controller
+{
+    public function __construct(){
+    	$this->myClient = new Client();
+        $this->apiURL = 'https://api.susi.ai/susi/chat.json?timezoneOffset=-420&q=';
+    }
+
+    /**
+     * Susi Get function
+     * @param url $url
+     * @param token_api @token
+     */
+    public function getFunction($query)
+    {
+        $request = $this->myClient->get($this->apiURL.$query);
+        $response = $request->getBody();
+        $data = json_decode($response, true);
+        return $data['answers'][0]['actions'][0]['expression'];
+    }
+}
